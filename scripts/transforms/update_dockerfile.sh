8b883e64e7154299eae0267210274237360a1e08
Revital Sur <eres@il.ibm.com>
2024-06-25 23:56:00 +0300

    e

--- scripts/transforms/update_dockerfile.sh
+++ scripts/transforms/update_dockerfile.sh
@@ -5,7 +5,7 @@
 set -euo pipefail
 
 if [[ $# != 1 ]]; then
-  cat << EOF
+  cat <<EOF
         "Incorrect number of parameters provided. The required parameter is ray version.
 EOF
   exit 1
@@ -13,10 +13,50 @@ fi
 
 ray=$1
 
-first_line=$(head -n 1 Dockerfile)
-if [[ $first_line == *" AS "* ]]; then
-	as_expr=$(echo $first_line | grep -Eo "AS ([a-z]+)")
-	sed -i.back "s%FROM docker.io/rayproject/ray:.*%FROM docker.io/rayproject/ray:$ray\-py310 $as_expr%" Dockerfile
-else
-        sed -i.back "s%FROM docker.io/rayproject/ray:.*%FROM docker.io\/rayproject/ray:$ray\-py310%" Dockerfile
-fi
+update_dockerfile() {
+  first_line=$(head -n 1 Dockerfile)
+  if [[ $first_line == *" AS "* ]]; then
+    as_expr=$(echo $first_line | grep -Eo "AS ([a-z]+)")
+    sed -i.back "s%FROM docker.io/rayproject/ray:.*%FROM docker.io/rayproject/ray:$ray\-py310 $as_expr%" Dockerfile
+  else
+    sed -i.back "s%FROM docker.io/rayproject/ray:.*%FROM docker.io\/rayproject/ray:$ray\-py310%" Dockerfile
+  fi
+}
+
+check_dockerfile() {
+  first_line=$(head -n 1 Dockerfile)
+  if [[ $first_line == *" AS "* ]]; then
+    as_expr=$(echo $first_line | grep -Eo "AS ([a-z]+)")
+    expected_task="FROM docker.io/rayproject/ray:$ray-py310 $as_expr"
+    if [[ $first_line != $expected_task ]]; then
+				echo $first_line $expected_task
+				exit 1
+			fi
+  else
+    expected_task="FROM docker.io/rayproject/ray:$ray-py310"
+    if [[ $first_line != $expected_task ]]; then
+				echo $first_line $expected_task
+				exit 1
+			fi
+  fi
+}
+
+usage() {
+  cat <<EOF
+"Usage: ./update_dokcerfiles.sh [check_dokcerfile|update_dokcerfile]"
+EOF
+}
+
+case "$op" in
+check_dockerfile)
+  header_text "Check dokcerfile in workflow"
+  check_dokcerfile $1 $2
+  ;;
+update_dockerfile)
+  header_text "Update dokcerfile in workflow"
+  update_dockerfile $1 $2
+  ;;
+*)
+  usage
+  ;;
+esac
--- scripts/transforms/update_workflow_tags.sh
+++ scripts/transforms/update_workflow_tags.sh
@@ -1,79 +1,81 @@
 #!/usr/bin/env bash
 
-op=$1; shift
-set -x
+op=$1
+shift
+
+if [[ $# != 2 ]]; then
+  cat << EOF
+	"Incorrect number of parameters provided. The required parameters are versions_file and pipeline_path. 
+EOF
+  exit 1
+fi
+
+versions_file=$1
+pipeline_path=$2
+
 source ../common.sh
 
 update_tags() {
-versions_file=$1
-pipeline_path=$2
 
-# Modify the tasks tags as defined in the versions file
-while IFS= read -r line; do 
-	[ -z "$line" ] && continue
-	[[ $line == *#* ]] && continue
-	[[ $line == *ifeq* || $line == *else* || $line == *endif* ]] && continue
-	VERSION_NAME=$(echo $line |cut -d "=" -f 1)
-	DOCKER_IMAGE_NAME=$(echo $line |cut -d "=" -f 1 |sed "s/_VERSION//" |tr '[:upper:]' '[:lower:]')
-	DOCKER_IMAGE_NAME=$(echo $DOCKER_IMAGE_NAME |sed "s/_ray$/\-ray/" | sed "s/_spark$/\-spark/" | sed "s/_parquet$/\-parquet/")
-	DOCKER_IMAGE_VERSION=$(eval echo ${!VERSION_NAME})
-	sed -i.back "s/data-prep-kit\/$DOCKER_IMAGE_NAME:.*/data-prep-kit\/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION\"/" $pipeline_path
-done < $versions_file
+	# Modify the tasks tags as defined in the versions file
+	while IFS= read -r line; do
+		[ -z "$line" ] && continue
+		[[ $line == *#* ]] && continue
+		[[ $line == *ifeq* || $line == *else* || $line == *endif* ]] && continue
+		VERSION_NAME=$(echo $line | cut -d "=" -f 1)
+		DOCKER_IMAGE_NAME=$(echo $line | cut -d "=" -f 1 | sed "s/_VERSION//" | tr '[:upper:]' '[:lower:]')
+		DOCKER_IMAGE_NAME=$(echo $DOCKER_IMAGE_NAME | sed "s/_ray$/\-ray/" | sed "s/_spark$/\-spark/" | sed "s/_parquet$/\-parquet/")
+		DOCKER_IMAGE_VERSION=$(eval echo ${!VERSION_NAME})
+		sed -i.back "s/data-prep-kit\/$DOCKER_IMAGE_NAME:.*/data-prep-kit\/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION\"/" $pipeline_path
+	done <$versions_file
 }
 
-check_tags(){
-versions_file=$1
-pipeline_path=$2
-
-# Modify the tasks tags as defined in the versions file
-while IFS= read -r line; do
-        [ -z "$line" ] && continue
-        [[ $line == *#* ]] && continue
-        [[ $line == *ifeq* || $line == *else* || $line == *endif* ]] && continue
-	echo $line
-        VERSION_NAME=$(echo $line |cut -d "=" -f 1)
-	echo "hi"
-        DOCKER_IMAGE_NAME=$(echo $line |cut -d "=" -f 1 |sed "s/_VERSION//" |tr '[:upper:]' '[:lower:]')
-	echo "hi"
-        echo "hi"
-	DOCKER_IMAGE_NAME=$(echo $DOCKER_IMAGE_NAME |sed "s/_ray$/\-ray/" | sed "s/_spark$/\-spark/" | sed "s/_parquet$/\-parquet/")
-	echo "hi"
-        DOCKER_IMAGE_VERSION=$(eval echo ${!VERSION_NAME})
-	echo "hi5"
-	echo $DOCKER_IMAGE_NAME
-	echo $pipeline_path
-	task_name="$(grep $DOCKER_IMAGE_NAME $pipeline_path)"
-	echo $task_name "task_name"
-	if [[ $task_name != "" ]]; then
-		task_in_workflow=$(grep "task_image =" $pipeline_path | sed 's%task_image =%%'|sed 's/"//g' |tr '[:upper:]' '[:lower:]')
-		expected_task="quay.io/dataprep1/data-prep-kit/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION"
-		if [[ $task_in_workflow != $expected_task ]]; then
-			echo $task_in_workflow $expected_task
-		else
-			echo $task_in_workflow $expected_task
-		fi	
+check_tags() {
+	task_found=0
+	# Modify the tasks tags as defined in the versions file
+	while IFS= read -r line; do
+		[ -z "$line" ] && continue
+		[[ $line == *#* ]] && continue
+		[[ $line == *ifeq* || $line == *else* || $line == *endif* ]] && continue
+		[[ $line == RAY=* ]] && continue
+		VERSION_NAME=$(echo $line | cut -d "=" -f 1)
+		DOCKER_IMAGE_NAME=$(echo $line | cut -d "=" -f 1 | sed "s/_VERSION//" | tr '[:upper:]' '[:lower:]')
+		DOCKER_IMAGE_NAME=$(echo $DOCKER_IMAGE_NAME | sed "s/_ray$/\-ray/" | sed "s/_spark$/\-spark/" | sed "s/_parquet$/\-parquet/")
+		DOCKER_IMAGE_VERSION=$(eval echo ${!VERSION_NAME})
+		task_name="$(grep $DOCKER_IMAGE_NAME $pipeline_path)"
+		
+		if [[ $task_name != "" ]]; then
+			task_found=1
+			task_in_workflow=$(grep "task_image =" $pipeline_path | sed 's%task_image =%%' | sed 's/"//g' | tr '[:upper:]' '[:lower:]' | tr -d ' ')
+			expected_task="quay.io/dataprep1/data-prep-kit/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION"
+			if [[ $task_in_workflow != $expected_task ]]; then
+				echo "unexpected tag $task_in_workflow $expected_task"
+				exit 1
+			fi
+		fi
+	done <$versions_file
+	if [[ $task_found -eq 0 ]]; then
+		echo "transform image missing from $pipeline_path"
+		exit 1
 	fi
-done < $versions_file
 }
 
-
-
-usage(){
-        cat <<EOF
+usage() {
+	cat <<EOF
 "Usage: ./update_workflow_tags.sh [check_tags|update_tags]"
 EOF
 }
 
 case "$op" in
 check_tags)
-  header_text "Check tags in workflow"
-  check_tags $1 $2
-  ;;
+	header_text "Check tags in workflow"
+	check_tags
+	;;
 update_tags)
-  header_text "Update tags in workflow"
-  update_tags
-  ;;
- *)
-  usage
-  ;;
+	header_text "Update tags in workflow"
+	update_tags
+	;;
+*)
+	usage
+	;;
 esac
